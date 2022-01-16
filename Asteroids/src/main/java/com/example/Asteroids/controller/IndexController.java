efed34d758daf21ed21a49f12c61438f2f5ae471
walterbaya <walterbaya@yahoo.com>
2022-01-16 11:56:40 -0300

    se muestra por pantalla la cantidad de asteroides

--- Asteroids/src/main/java/com/example/Asteroids/controller/IndexController.java
+++ Asteroids/src/main/java/com/example/Asteroids/controller/IndexController.java
@@ -5,7 +5,6 @@ import com.example.Asteroids.model.Asteroid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 
@@ -16,7 +15,7 @@ public class IndexController {
     public String greeting(Model model) throws Exception {
         ArrayList<Asteroid> asteroids = RestClient.getAsteroids("2022-01-1","2022-01-7");
         model.addAttribute("asteroidName", asteroids.get(0).toString());
-        model.addAttribute("numberOfAsteroids", asteroids.size());
+        model.addAttribute("numberOfAsteroids", String.valueOf(asteroids.size()));
         return "index";
     }
 
