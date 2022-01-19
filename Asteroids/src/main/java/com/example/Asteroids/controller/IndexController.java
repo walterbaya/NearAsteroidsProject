12a1f0c96791938ce93c129fd41580e8b9d9d0a5
walterbaya <walterbaya@yahoo.com>
2022-01-19 19:16:15 -0300

    se agrega funcionamiento de obtencion de datos con thymeleaf

--- Asteroids/src/main/java/com/example/Asteroids/controller/IndexController.java
+++ Asteroids/src/main/java/com/example/Asteroids/controller/IndexController.java
@@ -2,18 +2,31 @@ package com.example.Asteroids.controller;
 
 import com.example.Asteroids.Client.RestClient;
 import com.example.Asteroids.model.Asteroid;
+import com.example.Asteroids.model.TimeInterval;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.thymeleaf.expression.Dates;
 
+import java.sql.Time;
 import java.util.ArrayList;
 
 @Controller
 public class IndexController {
 
+
     @GetMapping("/")
-    public String greeting(Model model) throws Exception {
-        ArrayList<Asteroid> asteroids = RestClient.getAsteroids("2022-01-9","2022-01-16");
+    public String greeting(Model model) {
+        model.addAttribute("timeInterval", new TimeInterval());
+        return "index";
+    }
+
+    @PostMapping("/setTimeInterval")
+    public String greetingSubmit(@ModelAttribute TimeInterval timeInterval, Model model) throws Exception {
+        model.addAttribute("timeInterval", timeInterval);
+        ArrayList<Asteroid> asteroids = RestClient.getAsteroids(timeInterval.getFrom(), timeInterval.getTo());
         model.addAttribute("asteroidName", asteroids.get(0).toString());
         model.addAttribute("numberOfAsteroids", String.valueOf(asteroids.size()));
         model.addAttribute("asteroids", asteroids);
---
+++ Asteroids/src/main/java/com/example/Asteroids/model/TimeInterval.java
@@ -0,0 +1,9 @@
+package com.example.Asteroids.model;
+
+import lombok.Data;
+
+@Data
+public class TimeInterval {
+    private String from;
+    private String to;
+}
