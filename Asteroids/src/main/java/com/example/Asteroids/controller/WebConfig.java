1199b75eb298f77776e36eeb3d5b8d1596b99b51
walterbaya <walterbaya@yahoo.com>
2022-02-09 20:44:00 -0300

    se agregan robots de busqueda

---
+++ Asteroids/src/main/java/com/example/Asteroids/controller/WebConfig.java
@@ -0,0 +1,13 @@
+package com.example.Asteroids.controller;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+public class WebConfig extends WebMvcConfigurationSupport {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/robots.txt").addResourceLocations("/static/robots.txt");
+    }
+}
